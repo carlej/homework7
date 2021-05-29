e0a756dfcc50c54a8efb96cb78e6deaf10d0ce5e
carlej <carlej@oregonstate.edu>
2021-05-29 03:06:22 -0700

    Implemented divide 100 method
    made it so that all years divisible by both 4 and 100 are not included as leap years

--- leapYear.py
+++ leapYear.py
@@ -1,6 +1,9 @@
 def LY(a):
     year = int(a)
     if year%4 == 0:
-        return True
+        if year%100 == 0:
+            return False
+        else:
+            return True
     else:
         return False
