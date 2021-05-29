770ee2786fd305f2ae00bee0193bad9ac93d27ec
carlej <carlej@oregonstate.edu>
2021-05-29 03:02:58 -0700

    Implemented divide by 4 method
    Made it so that all years divisible by 4 are returned as leap years

---
+++ leapYear.py
@@ -0,0 +1,6 @@
+def LY(a):
+    year = int(a)
+    if year%4 == 0:
+        return True
+    else:
+        return False
--- test_leapYear.py
+++ test_leapYear.py
@@ -1,7 +1,6 @@
 import leapYear
 
 
-def test_output(capsys):
-    leapYear.LY()
+def test_output():
     assert leapYear.LY(2020) == True
     assert leapYear.LY(2021) == False
